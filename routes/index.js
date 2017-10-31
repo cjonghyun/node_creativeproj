b9e3a81cc8497e396a3d3d2b9f710c2ce0bd4d4d
cjonghyun <cjonghyun@github.com>
2017-10-31 06:05:06 +0000

    lab 4 complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
+var fs = require('fs');
+var https = require('https');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+      res.sendFile('weather.html', { root:  'public' });
+});
+
+router.get('/getcity', function(req, res, next) {
+  fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
+    if(err) throw err;
+    var jsonresult = [];
+    var cities = data.toString().split("\n");
+    var searchStr = req.query.q;
+    if(searchStr)
+      var myRe = new RegExp("^" + req.query.q.toLowerCase());
+    else
+      var myRe = new RegExp("^");
+    for(var i = 0; i < cities.length; i++) {
+      var result = cities[i].toLowerCase().search(myRe);
+      if (result != -1){
+        console.log(cities[i]);
+        jsonresult.push({city:cities[i]}); 
+    }
+  }
+  res.status(200).json(jsonresult);
+  });	
+});
+
+router.get('/getWord', function(req, res, next){
+  var result = "";
+  https.get('https://owlbot.info/api/v1/dictionary/' + req.query.q, function(response){
+    response.on('data', function(d) {
+      result += d;
+    });
+    response.on('end', function(){
+      res.status(200).json(JSON.parse(result));
+    });
+  });  
 });
 
 module.exports = router;
